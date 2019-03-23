624a51642c2c756b1ec89fcc9cb1652a94d18e9b
lorenzo <brainstew927@gmail.com>
2019-03-23 23:01:53 +0100

    inserito login

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
+
+var user_glob;
+
 function start() {
-    console.log("hey, chiamato");
+    console.log("avviato js");
 }
 
-function butt_click(text) {
-    console.log("premuto il bottone:"+text)
+function butt_click(pass, user) {
+    console.log("premuto il bottone");
+
+    try_log(user, pass);
+}
+
+function ret_user() {
+    return user_glob;
+}
+
+function try_log(user, pass) {
+    console.log("tentando di accedere con:");
+    console.log("\tuser:  " + user);
+    console.log("\tpass:  " + pass);
+
+    if (user == "brain" && pass == "lol") {
+
+        user_glob = user;
+
+        textBx = document.getElementById("err");
+/*
+        textBx.hidden = false;
+        textBx.isContentEditable = true;
+        textBx.textContent = "ciao " + user;
+*/      
+        window.location.href = "sec.html"
+    }
+}
+
+function sec_hand(user) {
+    console.log("sosss:" + user);
 }
